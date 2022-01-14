6ae8fad6185d8e364d5c73238acbddb9d902466f
Laurence279 <lnunn279@gmail.com>
2022-01-14 14:29:14 +0000

    Create dropdown categories from api

--- src/App.js
+++ src/App.js
@@ -4,37 +4,56 @@ import Button from "./components/button";
 import Dropdown from "./components/dropdown";
 
 function App() {
+
   const [joke, setJoke] = useState("");
 
-  const [category, setCategory] = useState("animal");
+  const [category, setCategory] = useState("random");
+  const [categoryList, setCategoryList] = useState([]) // 1. Set a state for the categories, start it as an empty array..
 
-  async function fetchDataCategory() {
+  async function fetchCategories(){
+    const response = await fetch(`https://api.chucknorris.io/jokes/categories`);
+    const data = await response.json();
+    setCategoryList(data) //3. Sets the categoryList state to be whatever was returned in the json..
+  }
+
+  useEffect(() => {
+    fetchData();
+    fetchCategories(); //2. Gets called once when page is loaded
+  }, []);
+
+  async function fetchData() {
+
+    if(category === "random"){
+      const response = await fetch("https://api.chucknorris.io/jokes/random");
+      const data = await response.json();
+      return(data.value);
+    }
+    
     const response = await fetch(
       `https://api.chucknorris.io/jokes/random?category=${category}`
     );
     const data = await response.json();
-    setJoke(data.value);
+    return (data.value);
+
   }
 
-  async function fetchData() {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    const data = await response.json();
-    setJoke(data.value);
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  function jokeGetter() {
-    fetchData();
+  async function jokeGetter() {
+    const joke = await fetchData();
+    setJoke(joke)
   }
 
+  function handleChange(event){
+    const category = event.target.value;
+    setCategory(category)
+  }
+
+  //3. Send the state as a prop to Dropdown component
   return (
     <div>
       <h1>Chuck Norris Joke Generator</h1>
-      <Button handleClick={jokeGetter} buttonText="Get Random Joke" />
-      <Button handleClick={jokeGetter} buttonText="James" />
-      <Dropdown />
+      <Button handleClick={jokeGetter} buttonText="Get Joke"/>
+      <Button handleClick={jokeGetter} buttonText="todo.." />
+      <Dropdown handleChange={handleChange} options={categoryList}/>
       <p>{joke}</p>
     </div>
   );
