00d6a86cb6e6be6ccb79bfefcc03c81add0f0c48
ravigordon <rgordz460@outlook.com>
2022-01-14 11:27:55 +0000

    button for jokes working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [joke,setJoke] = useState("");
+
+
+  async function fetchData(){
+    const response = await fetch('https://api.chucknorris.io/jokes/random')
+    const data = await response.json();
+    setJoke(data.value);
+  }
+  useEffect(() =>{
+    fetchData();
+  },[])
+
+  function handleClick(){
+     fetchData();
+  }
+
+
+return(
+  <div>
+      <h1>Chuck Norris Joke Generator</h1>
+      <button onClick={handleClick}>click me</button>
+      <p>{joke}</p>   
+  </div>
+  
+)
+   
 }
 
 export default App;
